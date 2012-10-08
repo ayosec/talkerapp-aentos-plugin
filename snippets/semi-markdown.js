604727f856ff2fb04869c13109d0e74bd72ec749
Ayose <ayosec@gmail.com>
2012-10-08 15:21:52 +0100

    Fix bug when use the format code several times in a message

--- snippets/semi-markdown.js
+++ snippets/semi-markdown.js
@@ -1,6 +1,6 @@
 
-var reCode = /`(\S+(?:\s+\S+)*)`/;
-var reBold = /\*(\S+(?:\s+\S+)*)\*/;
+var reCode = /`(\S+?(?:\s+\S+?)*?)`/g;
+var reBold = /\*(\S+?(?:\s+\S+?)*?)\*/g;
 
 var lastInsertion = Talker.getLastInsertion();
 lastInsertion.replace(reCode, "<tt style='background: #eee'>$1</tt>");
